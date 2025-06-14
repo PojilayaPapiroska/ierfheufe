d8cd86ca9ec281fc76e4c091ff9616b793a225c2
PojilayaPapiroska <barabulkins95@gmail.com>
2025-06-14 16:16:03 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,12 @@
-const tg = window.Telegram.WebApp;
+let tg = window.Telegram.WebApp;
+
 document.getElementById("sendBtn").addEventListener("click", () => {
   const amount = parseInt(document.getElementById("amount").value);
   if (!amount || amount <= 0) {
-    alert("Введите корректное число");
+    alert("Введите корректное число звёзд");
     return;
   }
-  tg.sendData(JSON.stringify({ amount }));
-  tg.close();
+
+  tg.sendData(JSON.stringify({ amount }));  // передаём в бот
+  document.body.innerHTML = `<h2 style="color:white">✅ Промокод формируется. Смотрите в Telegram.</h2>`;
 });
